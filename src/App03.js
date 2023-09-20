3e2e99be976f7a56b92279c0bd2ded4994cf7e2b
九级浪 <413612464@qq.com>
2023-09-20 19:05:06 +0800

    feat:使用react-three-fiber库

--- src/App03.js
+++ src/App03.js
@@ -1,8 +1,18 @@
+import { Canvas } from '@react-three/fiber';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <Canvas>
+        <mesh>
+          <boxGeometry />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight args={[0xffffff]} intensity={0.5} />
+        <directionalLight args={[0xffffff]} position={[0, 5, 5]} intensity={0.5} />
+      </Canvas>
     </div>
   );
 }
