5fc56cea4cbb4eec1d07bee804e1a7216cd3867e
九级浪 <413612464@qq.com>
2023-09-20 18:36:17 +0800

    feat:函数式组件引用three_初始化

---
+++ src/App02.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'></div>
+  );
+}
+
+export default App;
