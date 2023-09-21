cbf395b70f4c108c5008a8ff5beb48c7d74651fc
九级浪 <413612464@qq.com>
2023-09-21 11:05:06 +0800

    feat:事件交互与事件对象属性_初始化

---
+++ src/App07.js
@@ -0,0 +1,45 @@
+/**
+ * 07.事件交互与事件对象属性
+ */
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Canvas>
+        <InfoThree></InfoThree>
+        <BoxRotation></BoxRotation>
+        <ambientLight args={[0xffffff]} intensity={0.5} />
+        <directionalLight args={[0xffffff]} position={[0, 5, 5]} intensity={0.5} />
+      </Canvas>
+    </div>
+  );
+}
+
+function BoxRotation() {
+  const mesh = useRef();
+
+  useFrame(({ clock }) => {
+    mesh.current.rotation.x = clock.getElapsedTime();
+    mesh.current.rotation.y = clock.getElapsedTime();
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry />
+      <meshPhongMaterial />
+    </mesh>
+  );
+}
+
+function InfoThree() {
+  const three = useThree();
+  const { camera, gl } = three;
+  console.log(three, camera, gl);
+}
+
+export default App;
