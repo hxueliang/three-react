5fdb068cc8612601a25b6d517ce44de8c5a68d6b
九级浪 <413612464@qq.com>
2023-09-21 11:38:14 +0800

    feat:Environment与背景设置_初始化

---
+++ src/App09.js
@@ -0,0 +1,39 @@
+/**
+ * 09.useLoader与背景设置
+ */
+import { Canvas, useFrame, useThree, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from '@react-three/drei';
+import { useRef, Suspense } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Canvas camera={{ position: [0, 2, 5] }}>
+        <InfoThree></InfoThree>
+        <OrbitControls></OrbitControls>
+        <ambientLight args={[0xffffff]} intensity={0.5} />
+        <directionalLight args={[0xffffff]} position={[0, 5, 5]} intensity={0.5} />
+
+        <Suspense>
+          <Model></Model>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
+
+function Model() {
+  const gltf = useLoader(GLTFLoader, './assets/model/pad.gltf');
+  return <primitive object={gltf.scene} scale="10" />;
+}
+
+function InfoThree() {
+  const three = useThree();
+  const { camera, gl } = three;
+  console.log(three, camera, gl);
+}
+
+export default App;
