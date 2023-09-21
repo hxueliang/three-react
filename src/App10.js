01bb8810226bac02e4cc0d775555162bd5c53dd5
九级浪 <413612464@qq.com>
2023-09-21 12:24:31 +0800

    feat:纹理材质

--- src/App10.js
+++ src/App10.js
@@ -5,6 +5,7 @@ import { Canvas, useFrame, useThree, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls, Environment } from '@react-three/drei';
 import { useRef, Suspense } from 'react';
+import * as THREE from 'three';
 
 import './App.css';
 
@@ -18,10 +19,38 @@ function App() {
         <directionalLight args={[0xffffff]} position={[0, 5, 5]} intensity={0.5} />
 
         <Suspense fallback={null}>
+          <Model></Model>
         </Suspense>
       </Canvas>
     </div>
   );
+}
+
+function Model() {
+  const BASE_URL = './assets/texture/';
+  const [
+    colorMap,
+    displacementMap,
+    normalMap,
+    roughnessMap,
+    aoMap
+  ] = useLoader(THREE.TextureLoader, [
+    `${BASE_URL}PavingStones092_1K_Color.jpg`,
+    `${BASE_URL}PavingStones092_1K_Displacement.jpg`,
+    `${BASE_URL}PavingStones092_1K_Normal.jpg`,
+    `${BASE_URL}PavingStones092_1K_Roughness.jpg`,
+    `${BASE_URL}PavingStones092_1K_AmbientOcclusion.jpg`,
+  ]);
+  return <mesh>
+    <sphereGeometry args={[1, 100, 100]}></sphereGeometry>
+    <meshStandardMaterial
+      map={colorMap}
+      displacementMap={displacementMap} displacementScale={0.5}
+      normalMap={normalMap}
+      roughnessMap={roughnessMap}
+      aoMap={aoMap}
+    />
+  </mesh>;
 }
 
 function InfoThree() {
