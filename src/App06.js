2b0b02c34230f55b366ad9b8071ec736163480ac
九级浪 <413612464@qq.com>
2023-09-21 10:58:57 +0800

    feat:useThree查看three信息_初始化

---
+++ src/App06.js
@@ -0,0 +1,38 @@
+/**
+ * 06.useThree与事件交互
+ */
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Canvas>
+        <BoxRotation></BoxRotation>
+        <ambientLight args={[0xffffff]} intensity={0.5} />
+        <directionalLight args={[0xffffff]} position={[0, 5, 5]} intensity={0.5} />
+      </Canvas>
+    </div>
+  );
+}
+
+function BoxRotation() {
+  const mesh = useRef();
+
+  useFrame(({ clock }) => {
+    mesh.current.rotation.x = clock.getElapsedTime();
+    mesh.current.rotation.y = clock.getElapsedTime();
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry />
+      <meshPhongMaterial />
+    </mesh>
+  );
+}
+
+export default App;
