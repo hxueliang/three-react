eae24699a9ad75bfe10ac6c97e927d6e7009aea4
九级浪 <413612464@qq.com>
2023-12-12 06:24:24 +0800

    chore: 构架调整

---
+++ src/router/common.js
@@ -0,0 +1,14 @@
+import App from '../App';
+
+const router0 = [
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '*',
+    element: <div>404</div>
+  },
+];
+
+export default router0;
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import router0 from './common';
+
+const router = createBrowserRouter([
+  ...router0,
+]);
+
+export default router;
